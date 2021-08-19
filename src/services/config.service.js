247ce8e254b2877fd666bdf013d085515248498a
unknown <israelroddiniz@gmail.com>
2021-08-19 14:31:48 +0100

    changed back-end URL

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,7 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'http://localhost:8001/';
+    service.apiBase = 'http://quizapiird-env-1.eba-b2ub2knd.us-east-2.elasticbeanstalk.com/';
 
     return service;
 }]);
